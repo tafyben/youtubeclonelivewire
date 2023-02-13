74569f29ca238d0fc0e22f437c1e32216b3fe763
Tafadzwa Ben <tbmukwepa@gmail.com>
2023-02-13 16:09:26 +0200

    channel user relationship

--- app/Models/Channel.php
+++ app/Models/Channel.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Channel extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(Channel::class);
+    }
 }
