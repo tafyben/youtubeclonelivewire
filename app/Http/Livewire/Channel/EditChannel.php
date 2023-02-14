478dc181bfdb2aecc78fb66d7ea14782723cac05
Tafadzwa Ben <tbmukwepa@gmail.com>
2023-02-14 14:23:03 +0200

    created livewire channel component files

---
+++ app/Http/Livewire/Channel/EditChannel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Channel;
+
+use Livewire\Component;
+
+class EditChannel extends Component
+{
+    public function render()
+    {
+        return view('livewire.channel.edit-channel');
+    }
+}
