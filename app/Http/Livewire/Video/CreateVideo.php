e260afb2a51c8bed7152130482e0e5d4e17e7270
Tafadzwa Ben <tbmukwepa@gmail.com>
2023-02-16 14:37:56 +0200

    created livewire createvideo component

---
+++ app/Http/Livewire/Video/CreateVideo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Video;
+
+use Livewire\Component;
+
+class CreateVideo extends Component
+{
+    public function render()
+    {
+        return view('livewire.video.create-video');
+    }
+}
