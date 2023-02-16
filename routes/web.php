a383a31bdf9020fc60323573c992bb738e470203
Tafadzwa Ben <tbmukwepa@gmail.com>
2023-02-16 14:44:12 +0200

    added video routes to web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ChannelController;
 use App\Http\Controllers\HomeController;
+use App\Http\Livewire\Video\AllVideo;
+use App\Http\Livewire\Video\CreateVideo;
+use App\Http\Livewire\Video\EditVideo;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,4 +29,10 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::middleware('auth')->group(function () {
 
     Route::get('/channel/{channel}/edit', [ChannelController::class, 'edit'])->name('channel.edit');
+});
+Route::middleware('auth')->group(function () {
+
+    Route::get('/videos/{channel}/create', CreateVideo::class)->name('video.create');
+    Route::get('/videos/{channel}/{video}/edit', EditVideo::class)->name('video.edit');
+    Route::get('/videos/{channel}', AllVideo::class)->name('video.all');
 });
