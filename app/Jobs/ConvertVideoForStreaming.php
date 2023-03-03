48449a22380dd63ec5d326a6ef80ad62529dd6a1
Tafadzwa Ben <tbmukwepa@gmail.com>
2023-03-03 11:17:43 +0200

    added details to jobs convert video for streaming

--- app/Jobs/ConvertVideoForStreaming.php
+++ app/Jobs/ConvertVideoForStreaming.php
@@ -2,12 +2,17 @@
 
 namespace App\Jobs;
 
+use App\Models\Video;
+use FFMpeg\Format\Video\X264;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use ProtoneMedia\LaravelFFMpeg\Support\FFMpeg;
 
 class ConvertVideoForStreaming implements ShouldQueue
 {
@@ -18,9 +23,10 @@ class ConvertVideoForStreaming implements ShouldQueue
      *
      * @return void
      */
-    public function __construct()
+    public $video;
+    public function __construct(Video $video)
     {
-        //
+        $this->video = $video;
     }
 
     /**
@@ -30,6 +36,33 @@ class ConvertVideoForStreaming implements ShouldQueue
      */
     public function handle()
     {
-        //
+        $destination = '/' . $this->video->uid . '/' . $this->video->uid . '.m3u8';
+        $low = (new X264('aac'))->setKiloBitrate(500);
+        $high = (new X264('aac'))->setKiloBitrate(1000);
+
+        FFMpeg::fromDisk('videos-temp')
+            ->open($this->video->path)
+            ->exportForHLS()
+            ->addFormat($low, function ($filters) {
+                $filters->resize(640, 480);
+            })
+            ->addFormat($high, function ($filters) {
+                $filters->resize(1280, 720);
+            })
+            ->onProgress(function ($progress) {
+                $this->video->update([
+                    'processing_percentage' => $progress
+                ]);
+            })
+            ->toDisk('videos')
+            ->save($destination);
+
+        $this->video->update([
+            'processed' => true,
+            'proccessed_file' => $this->video->uid . '.m3u8'
+        ]);
+        //delete temp video
+        $result = Storage::disk('videos-temp')->delete($this->video->path);
+        Log::info($this->video->path . ' video was deleted from videos-temp folder');
     }
 }
