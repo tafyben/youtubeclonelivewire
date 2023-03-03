46c88e992f0b97547ea043a91af184f9512b01f5
Tafadzwa Ben <tbmukwepa@gmail.com>
2023-03-03 11:18:38 +0200

    added detail to create thumbnail for streaming

--- app/Jobs/CreateThumbnailFromVideo.php
+++ app/Jobs/CreateThumbnailFromVideo.php
@@ -2,12 +2,14 @@
 
 namespace App\Jobs;
 
+use App\Models\Video;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use ProtoneMedia\LaravelFFMpeg\Support\FFMpeg;
 
 class CreateThumbnailFromVideo implements ShouldQueue
 {
@@ -18,9 +20,10 @@ class CreateThumbnailFromVideo implements ShouldQueue
      *
      * @return void
      */
-    public function __construct()
+    public $video;
+    public function __construct(Video $video)
     {
-        //
+        $this->video = $video;
     }
 
     /**
@@ -30,6 +33,16 @@ class CreateThumbnailFromVideo implements ShouldQueue
      */
     public function handle()
     {
-        //
+        $destination = '/' . $this->video->uid . '/' . $this->video->uid . '.png';
+        FFMpeg::fromDisk('videos-temp')
+            ->open($this->video->path)
+            ->getFrameFromSeconds(2)
+            ->export()
+            ->toDisk('videos')
+            ->save($destination);
+
+        $this->video->update([
+            'thumbnail_image' => $this->video->uid . '.png'
+        ]);
     }
 }
